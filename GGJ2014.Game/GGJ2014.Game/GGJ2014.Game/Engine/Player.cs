f8f0bd6c654123cf4259c78f1c1cd15d0b5941dd
Chris Hawkins <chris@tinymachinecollective.com>
2014-01-25 18:49:53 +1100

    lives < 0 not = 0

--- GGJ2014.Game/GGJ2014.Game/GGJ2014.Game/Engine/Player.cs
+++ GGJ2014.Game/GGJ2014.Game/GGJ2014.Game/Engine/Player.cs
@@ -38,7 +38,7 @@ using Microsoft.Xna.Framework.Audio;
                 this.Effects.Add(new PainEffect());
                 (character as Monster).Destroy();
 
-                if (lives == 0)
+                if (lives <= 0)
                 {
                     InputFrozen = true;
                 }
@@ -50,7 +50,7 @@ using Microsoft.Xna.Framework.Audio;
             base.Update(gameTime);
             this.fade.Update(gameTime);
 
-            if (lives == 0 && !fade.Fading)
+            if (lives <= 0 && !fade.Fading)
             {
                 gameEnd.Update((float)gameTime.ElapsedGameTime.TotalMilliseconds);
 
