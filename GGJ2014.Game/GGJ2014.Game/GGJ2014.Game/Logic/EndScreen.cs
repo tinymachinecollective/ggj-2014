a138b2b61f6e5944973fb21881105bb20e8f23d3
Chris Hawkins <chris@tinymachinecollective.com>
2014-01-26 15:02:58 +1100

    endscreen

--- GGJ2014.Game/GGJ2014.Game/GGJ2014.Game/Logic/EndScreen.cs
+++ GGJ2014.Game/GGJ2014.Game/GGJ2014.Game/Logic/EndScreen.cs
@@ -10,7 +10,6 @@ namespace GGJ2014.Game.Logic
     public class EndScreen : Screen
     {
         private Texture2D splash;
-        private GameScreen gameScreen;
         private Fade fade;
         private int score;
         private MouseState lastMouseState;
@@ -18,13 +17,12 @@ namespace GGJ2014.Game.Logic
         public EndScreen(int score, GraphicsDevice device)
             : base(device)
         {
-            this.gameScreen = new GameScreen(this.Device);
             this.splash = BigEvilStatic.Content.Load<Texture2D>("endscreen");
             this.score = score;
             this.fade = new Fade();
             fade.FadeOut = true;
             this.fade.Initialize();
-            this.fade.OnComplete = () => BigEvilStatic.ScreenManager.OpenScreen(gameScreen);
+            this.fade.Start();
         }
 
         public override void Draw(Bounds bounds)
@@ -34,7 +32,7 @@ namespace GGJ2014.Game.Logic
             spriteBatch.Begin();
             spriteBatch.Draw(splash, Vector2.Zero, Color.White);
 
-            spriteBatch.DrawString(BigEvilStatic.Content.Load<SpriteFont>("ribeye"), score + " cat toys murdered", new Vector2(350, 600), Color.Black);
+            spriteBatch.DrawString(BigEvilStatic.Content.Load<SpriteFont>("ribeye"), score + " cat toys murdered", new Vector2(350, 590), Color.Black);
 
             fade.Draw(spriteBatch);
             spriteBatch.End();
@@ -45,7 +43,7 @@ namespace GGJ2014.Game.Logic
             this.fade.Update(time);
             var mouseState = Mouse.GetState();
 
-            if (mouseState.LeftButton == ButtonState.Pressed && !this.fade.Fading)
+            if (mouseState.LeftButton == ButtonState.Pressed && lastMouseState.LeftButton == ButtonState.Released && !this.fade.Fading)
             {
                 this.CloseUntil(typeof(SplashScreen));
             }
